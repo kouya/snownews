0f7c424f3ff2acea3d016ed21437f4e0fda0d691
Koz <koz.ross@retro-freedom.nz>
2017-10-24 11:37:27 +1300

    Relicensing to GPL-3.0

--- netio.c
+++ netio.c
@@ -7,7 +7,7 @@
  * netio.c
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
+ * it under the terms of the GNU General Public License version 3 as
  * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
